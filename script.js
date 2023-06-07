458eb568ecdaf6bb130b3c0f606fe15c4335040a
Jelena92370 <132999236+Jelena92370@users.noreply.github.com>
2023-06-07 22:34:19 +0200

    Add files via upload
    homework6 uploaded

---
+++ script.js
@@ -0,0 +1,30 @@
+// Задание 1
+
+// for(let i = 0; i < 10; i++) {
+//        if (i % 2 == 1) { 
+//     console.log(i)}
+//        }
+
+// Задание 2
+
+// for(let i = 55; i > 19; i--) {
+
+//     console.log(i)}
+    
+// Задание 3
+// const numbers = [3, 5, 11, 2, 8, 1, 6]
+// const numbers_squared = []
+// for(let i = 0; i < numbers.length; i++) {
+// numbers_squared.push(numbers[i] ** 2) 
+// }
+// const last_elem = (numbers_squared[6])
+
+// Задание 4
+const user = {
+    first_name: 'Ivan',
+    last_name: 'Ivanov',
+    age: 20,
+    salary: 500
+
+}
+console.log(`User's name is ${user.first_name} ${user.last_name}. He is ${user.age} years old.`)
